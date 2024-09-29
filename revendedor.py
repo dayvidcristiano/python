aaa8d363b85cb1532cf629c8a9a75e118eaa055f
Dayvid Cristiano <dayvidcristianosilva@gmail.com>
2024-09-29 16:12:21 -0300

    commit

--- revendedor.py
+++ revendedor.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-NumCarros = float(input("Digite o número de carros vendidos:"))
-ValorVendas = float(input("Valor total de vendas:"))
-SalarioFixo = float(input("Salário fixo:"))
-ComissaoFixa = float(input("Comissão por venda:"))
-=======
-#Uma revendedora de carros oferece aos seus vendedores um salário mensal fixo além de uma comissão fixa pelos carros vendido e 5% do valor total das vendas efetuadas por eles.
->>>>>>> eb35fb8 (pull)
-
 #Escrever um programa em Python que leia o número de carros por ele vendidos, o valor total de suas vendas, o salário fixo e a comissão que ele recebe por carro vendido. Calcule e escreva o salário final do vendedor.
 
 NumCarros = int(input("Digite o número de carros vendidos:"));
